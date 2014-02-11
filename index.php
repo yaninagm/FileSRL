8804785e6bcf27fb05e2fa94f84332a4dc8159c5
yanina <yanina@yanina-KB14>
2014-02-11 09:54:08 -0300

    estartegiasCalcular OrdenMerito y clase Seleccion

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE  html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/estilos.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/pestanias.css" type="text/css" media="screen" />
+        <!--[if IE 8]>
+                <link rel="stylesheet" type="text/css" media="screen" href="css/ie8-hacks.css" />
+        <![endif]-->
+        <!-- ENDS CSS -->	
+
+        <!-- GOOGLE FONTS 
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>-->
+
+        <!-- JS -->
+        <script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
+        <script type="text/javascript" src="js/easing.js"></script>
+        <script type="text/javascript" src="js/jquery.scrollTo-1.4.2-min.js"></script>
+        <script type="text/javascript" src="js/jquery.cycle.all.js"></script>
+        <script type="text/javascript" src="js/custom.js"></script>
+
+        <!-- Isotope -->
+        <script src="js/jquery.isotope.min.js"></script>
+
+        <!--[if IE]>
+                <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!--[if IE 6]>
+                <script type="text/javascript" src="js/DD_belatedPNG.js"></script>
+                <script>
+                /* EXAMPLE */
+                //DD_belatedPNG.fix('*');
+        </script>
+        <![endif]-->
+        <!-- ENDS JS -->
+
+
+        <!-- Nivo slider -->
+        <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+        <script src="js/nivo-slider/jquery.nivo.slider.js" type="text/javascript"></script>
+        <!-- ENDS Nivo slider -->
+
+
+
+        <!-- Fancybox -->
+        <link rel="stylesheet" href="js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+        <script type="text/javascript" src="js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+        <!-- ENDS Fancybox -->
+        
+    </head>
+
+    <body>
+
+        <div id="encabezado" >
+            
+        </div>
+        <div id="cuerpo">
+        <!-- ficheros -->
+        <div class="tabs" >
+            <div class="tab">
+                <input type="radio" id="tab-1" name="tab-group-1" checked>
+                <label for="tab-1">Inicio</label>
+                <div class="content"> 
+                    <!-- Slider -->
+                    <div id="slider-block">
+                        <div id="slider-holder">
+                            <div id="slider">
+                                <img src="images/01.jpg" title="" alt="" />
+                                <img src="images/02.jpg" title="" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <!-- ENDS Slider -->               
+                </div>
+            </div>
+            <div class="tab">
+                <input type="radio" id="tab-2" name="tab-group-1">
+                <label for="tab-2">Servicios</label>
+                <div class="content"> Soluciones a problemas de espacio y extravío de documentación</div>
+            </div>
+            <div class="tab">
+                <input type="radio" id="tab-3" name="tab-group-1">
+                <label for="tab-3">Contacto</label>
+                <div class="content"> el contenido de la pestaña 3 </div>
+            </div>
+            <div class="tab">
+                <input type="radio" id="tab-3" name="tab-group-1">
+                <label for="tab-3">Novedades</label>
+                <div class="content"> el contenido de la pestaña 3 </div>
+            </div>
+        </div>
+        <!-- Fin Fichero -->
+        </div>
+        <div id="pieDePagina">
+
+
+
+        </div> 
+
+    </body>
+</html>
---
+++ pestaniasNuevas/newEmptyPHP.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ 
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+ 
+<body>
+    <div id="page-wrap">
+        <ul id="main-nav">
+            <li class="current"><a href="index.html">Home</a></li>
+            <li class="portfolio"><a href="portfolio.html">Portfolio</a></li>
+            <li class="services"><a href="services.html">Services</a></li>
+            <li class="about"><a href="about.html">About Us</a></li>
+            <li class="contact"><a href="contact.html">Contact</a></li>
+        </ul>
+        <div class="clear"></div>
+        <div id="featured">
+            <div id="featured-content">
+            </div>
+        </div>
+    </div>
+</body>
+ 
+</html>
